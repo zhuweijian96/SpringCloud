e48c15dae03da397d0b78a09a930ba2c964a8c0a
朱伟健 <zhuwj@cloud-young.com>
2018-10-15 11:17:15 +0800

    整合mybatis，druid，log4j2

--- eurekaclient/src/main/java/com/example/eurekaclient/EurekaclientApplication.java
+++ eurekaclient/src/main/java/com/example/eurekaclient/EurekaclientApplication.java
@@ -1,7 +1,10 @@
 package com.example.eurekaclient;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
@@ -11,6 +14,9 @@ import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboar
 @EnableHystrix
 @EnableEurekaClient
 @SpringBootApplication
+@MapperScan("com.example.eurekaclient.mapper")
+@EnableAutoConfiguration
+@ServletComponentScan
 public class EurekaclientApplication {
 
     public static void main(String[] args) {
--- eurekaclient/src/main/java/com/example/eurekaclient/controller/ControllerTest.java
+++ eurekaclient/src/main/java/com/example/eurekaclient/controller/ControllerTest.java
@@ -1,6 +1,9 @@
 package com.example.eurekaclient.controller;
 
+import com.example.eurekaclient.model.ShopUser;
+import com.example.eurekaclient.service.ShopUserService;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,12 +14,20 @@ public class ControllerTest {
     @Value("${server.port}")
     private String port;
 
-    @HystrixCommand(fallbackMethod = "hiError")
+    @Autowired
+    ShopUserService shopUserService;
+
+    @HystrixCommand(fallbackMethod = "Error")
     @RequestMapping("hi")
     public String hi(){
         return port;
     }
-    public String hiError() {
+    public String Error() {
         return "sorry,error!";
+    }
+//    @HystrixCommand(fallbackMethod = "Error")
+    @RequestMapping("show")
+    public ShopUser show(){
+        return shopUserService.getById(1);
     }
 }
